8f244d8aeda2570e9f9eb635ae8ce67199360bf9
kis03160 <kis03160@naver.com>
2019-09-29 21:27:23 +0900

    과제 항목 추출 코드 수정

--- homework_crwal.py
+++ homework_crwal.py
@@ -9,41 +9,52 @@ id = browser.find_element_by_id("usr_id")
 id.send_keys("201600786")
 
 pw = browser.find_element_by_id("usr_pwd")
-pw.send_keys("")
+pw.send_keys("hwaitaeng1")
 
 login_bt=browser.find_element_by_class_name('btntype')
 login_bt.click()
 
-main_page = browser.page_source
 
-parse = bs(main_page, 'html.parser')
+# scheduel = browser.find_element_by_id('show_schedule_list')
+# scheduel.click()
 
+# main_page = browser.page_source
+#
+# parse = bs(main_page, 'html.parser')
+# divs = parse.findAll('span')
+# print(divs)
 # 과제 가져오기
 
 # 각 날짜 클릭
 # tds = parse.findAll("td", {"class" : ""})
 temp = 0
-# for tr in range(1, 6):
-#     for td in range(1, 8):
-        # date_click = browser.find_element_by_xpath('//*[@id="schedule"]/div[2]/table/tbody/tr[' + str(tr) +']/td[' + str(td) + ']')
-        # now_date = int(date_click.text)
-        #
-        # if now_date < temp:
-        #     break
-        # temp = now_date
-        # date_click.click()
-        # time.sleep(10)
+for tr in range(1, 6):
+    for td in range(1, 8):
+        date_click = browser.find_element_by_xpath('//*[@id="schedule"]/div[2]/table/tbody/tr[' + str(tr) +']/td[' + str(td) + ']')
+        now_date = int(date_click.text)
 
-browser.execute_script("getMainSchedule('','2019-9-6');")
-time.sleep(3)
-date_click = browser.find_element_by_xpath('//*[@id="schedule"]/div[2]/table/tbody/tr[1]/td[6]')
-time.sleep(3)
-date_click.click()
-time.sleep(3)
-divs = parse.find('div',{'class' : 'schedule-show-control'})
+        if now_date < temp:
+            break
+        temp = now_date
+        date_click.click()
+        time.sleep(1)
+        main_page = browser.page_source
 
+        parse = bs(main_page, 'html.parser')
+        divs = parse.find('div', {'class': 'schedule-show-control'})
 
-print(divs)
+        print(divs)
+
+# browser.execute_script("getMainSchedule('','2019-9-6');")
+# time.sleep(3)
+# date_click = browser.find_element_by_xpath('//*[@id="schedule"]/div[2]/table/tbody/tr[1]/td[6]')
+# time.sleep(3)
+# date_click.click()
+# time.sleep(3)
+# divs = parse.find('div',{'class' : 'schedule-show-control'})
+#
+#
+# print(divs)
 
 
 # month = "9"
