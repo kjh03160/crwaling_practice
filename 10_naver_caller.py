d22c6c5f516a314bc8e29a4615892ea3a596fd46
kjh03160 <kis03160@naver.com>
2019-10-01 10:28:02 +0900

    과제 가져오기 만드는중

--- 10_naver_caller.py
+++ 10_naver_caller.py
@@ -5,4 +5,4 @@ def call(keyword, start) :
     encText = quote(keyword)
     url = "https://openapi.naver.com/v1/search/blog?query=" + keyword  # json 결과
     result = requests.get(urlparse(url).geturl(),
-                          headers={"X-Naver-Client-Id": "lBPb8jMB4N1ngZP19Ls3", "X-Naver-Client-Secret": "3crCyo9t2v"})
+                          headers={"X-Naver-Client-Id": "", "X-Naver-Client-Secret": ""})
--- homework_crwal.py
+++ homework_crwal.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup as bs
 from selenium import webdriver
-import time
+import datetime
 
 browser = webdriver.Chrome('.\chromedriver_win32\chromedriver.exe')
 browser.get("http://eclass2.hufs.ac.kr:8181/ilos/main/member/login_form.acl")
@@ -15,47 +15,50 @@ login_bt=browser.find_element_by_class_name('btntype')
 login_bt.click()
 
 
-# scheduel = browser.find_element_by_id('show_schedule_list')
-# scheduel.click()
 
-# main_page = browser.page_source
-#
-# parse = bs(main_page, 'html.parser')
-# divs = parse.findAll('span')
-# print(divs)
 # 과제 가져오기
 
 # 각 날짜 클릭
-# tds = parse.findAll("td", {"class" : ""})
-temp = 0
+temp = 1
 for tr in range(1, 6):
     for td in range(1, 8):
         date_click = browser.find_element_by_xpath('//*[@id="schedule"]/div[2]/table/tbody/tr[' + str(tr) +']/td[' + str(td) + ']')
         now_date = int(date_click.text)
+        if now_date != temp:
+            continue
+        if now_date == temp:
+            date_click.click()
+            # time.sleep(1)
+            main_page = browser.page_source
+            parse = bs(main_page, 'html.parser')
+            divs = parse.find('div', {'class': 'schedule_txt_view'})
+            try:
+                hw_tags = divs.findAll('a')
+                hw_tag = None
+                for i in hw_tags:
+                    hw_tag = (i.text.splitlines())
+                if hw_tag == None:
+                    del hw_tag
+                else:               # 과목, 제목, 마감일 자르기 해야됨
+                    pass
+
+                # text_list = []
+                # div_text = divs.find('div').text.splitlines()
+                # for i in div_text:
+                #     if "[과제]" in text_list or i.strip() =="[과제]":
+                #          text_list.append(i.strip())
+                # if len(text_list) == 0:
+                #     del text_list
+                #
+                # now = datetime.datetime.now()
+                # nowDatetime = now.strftime('%Y-%m-%d %H:%M:%S')
+                # print(text_list)
+            except:
+                pass
 
         if now_date < temp:
             break
-        temp = now_date
-        date_click.click()
-        # time.sleep(1)
-        main_page = browser.page_source
-
-        parse = bs(main_page, 'html.parser')
-        divs = parse.find('div', {'class': 'schedule-show-control'})
-
-        print(divs)
-
-# browser.execute_script("getMainSchedule('','2019-9-6');")
-# time.sleep(3)
-# date_click = browser.find_element_by_xpath('//*[@id="schedule"]/div[2]/table/tbody/tr[1]/td[6]')
-# time.sleep(3)
-# date_click.click()
-# time.sleep(3)
-# divs = parse.find('div',{'class' : 'schedule-show-control'})
-#
-#
-# print(divs)
-
+        temp += 1
 
 # month = "9"
 # date_list = []
